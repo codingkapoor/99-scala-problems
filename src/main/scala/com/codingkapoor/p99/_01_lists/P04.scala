8372f582b7a1d61222ba504653c72b00e647fce9
Shivam Kapoor <mail@shivamkapoor.com>
2016-10-30 15:30:16 +0530

    solution to P04

---
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P04.scala
@@ -0,0 +1,32 @@
+package com.codingkapoor.p99._01_lists
+
+object P04 {
+
+  def lengthBuiltinI[A](ls: List[A]): Int = ls length
+
+  def lengthBuiltinII[A](ls: List[A]): Int = ls size
+
+  def lengthFunctionalI[A](ls: List[A]): Int = (ls foldLeft 0) { (acc, _) => acc + 1 }
+  
+  def lengthFunctionalII[A](ls: List[A]): Int = (ls.view foldLeft 0) { (acc, _) => acc + 1 }
+
+  def lengthFunctionalIII[A](ls: List[A]): Int = ls map { _ => 1 } sum
+  
+  def lengthFunctionalIV[A](ls: List[A]): Int = ls.view map { _ => 1 } sum
+
+  def lengthRecursiveI[A](ls: List[A]): Int = ls match {
+    case Nil     => 0
+    case _ :: xs => 1 + lengthRecursiveI(xs)
+  }
+
+  def lengthRecursiveII[A](ls: List[A]): Int = {
+
+    def lengthR(result: Int, list: List[A]): Int = list match {
+      case Nil     => result
+      case _ :: xs => lengthR(result + 1, xs)
+    }
+
+    lengthR(0, ls)
+  }
+  
+}
