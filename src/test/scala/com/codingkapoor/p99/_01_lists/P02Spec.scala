6163caf9527d68da3b80bb52faf0925eda11c20a
Shivam Kapoor <mail@shivamkapoor.com>
2016-10-30 12:45:52 +0530

    test cases for P02

---
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P02Spec.scala
@@ -0,0 +1,51 @@
+package com.codingkapoor.p99._01_lists
+
+import org.scalatest.{ FlatSpec, Matchers }
+
+class P02Spec extends FlatSpec with Matchers {
+
+  "secondLastElement" should "return the second last element from a list" in {
+    P02.secondLastElement(List(1, 2, 3, 4, 5)) should equal(4)
+  }
+
+  it should "throw NoSuchElementException when the list passed is empty" in {
+    intercept[NoSuchElementException] { P02.secondLastElement(Nil) }
+  }
+
+  "secondLastElementII" should "return the second last element from a list" in {
+    P02.secondLastElementII(List('a', 'b', 'c', 'd', 'e')) should equal('d')
+  }
+
+  it should "throw NoSuchElementException when the list passed is empty" in {
+    intercept[NoSuchElementException] { P02.secondLastElementII(List()) }
+  }
+
+  "secondLastBuiltin" should "return the second last element from a list" in {
+    P02.secondLastBuiltin(List("a", "b", "c")) should equal("b")
+  }
+
+  it should "throw NoSuchElementException when the list passed is empty" in {
+    intercept[NoSuchElementException] { P02.secondLastElementII(List.empty[String]) }
+  }
+
+  "secondLastBuiltinII" should "return the second last element from a list" in {
+    P02.secondLastBuiltin(List("a", "b", "c")) should equal("b")
+  }
+
+  it should "throw NoSuchElementException when the list passed is empty" in {
+    intercept[NoSuchElementException] { P02.secondLastElementII(List.empty[String]) }
+  }
+
+  "secondLastRecursive" should "return the second last element from a list" in {
+    P02.secondLastBuiltin(List("a", "b", "c")) should equal("b")
+  }
+
+  it should "throw NoSuchElementException when the list passed is empty" in {
+    intercept[NoSuchElementException] { P02.secondLastElementII(List.empty[String]) }
+  }
+
+  it should "throw NoSuchElementException when the list passed has only one element" in {
+    intercept[NoSuchElementException] { P02.secondLastElementII(List(1)) }
+  }
+
+}
