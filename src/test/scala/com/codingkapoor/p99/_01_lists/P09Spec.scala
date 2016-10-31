fbead76aa556577e993389ae6f58190df6670585
Shivam Kapoor <mail@shivamkapoor.com>
2016-10-31 23:19:57 +0530

    code format udpated for P09Spec

--- src/test/scala/com/codingkapoor/p99/_01_lists/P09Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P09Spec.scala
@@ -5,18 +5,19 @@ import org.scalatest.{ FlatSpec, Matchers }
 class P09Spec extends FlatSpec with Matchers {
 
   "packFunctional" should "return consecutive duplicate elements in a list packed into sublists." in {
-    P09.packFunctional(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal(List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
+    P09.packFunctional(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal (List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
   }
 
   "packRecursive" should "return consecutive duplicate elements in a list packed into sublists." in {
-    P09.packRecursive(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal(List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
+    P09.packRecursive(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal (List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
   }
 
   "packTailRecursive" should "return consecutive duplicate elements in a list packed into sublists." in {
-    P09.packTailRecursive(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal(List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
+    P09.packTailRecursive(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal (List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
   }
 
   "packTailRecursiveII" should "return consecutive duplicate elements in a list packed into sublists." in {
-    P09.packTailRecursiveII(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal(List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
+    P09.packTailRecursiveII(List(1, 1, 1, 2, 2, 1, 3, 3, 3, 4)) should equal (List(List(1, 1, 1), List(2, 2), List(1), List(3, 3, 3), List(4)))
   }
+  
 }
