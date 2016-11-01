98bf8559fde98da1f6be41a7395c308468905f76
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-01 21:10:46 +0530

    Solution and test cases for P15

--- src/main/scala/com/codingkapoor/p99/_01_lists/P15.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P15.scala
@@ -1,5 +1,21 @@
 package com.codingkapoor.p99._01_lists
 
 object P15 {
-  
+
+  def duplicateNFunctional[T](n: Int, ls: List[T]) = ls map { x => for (i <- 1 to n) yield x } flatten
+
+  def duplicateNFunctionalII[T](n: Int, ls: List[T]) = ls flatMap { x => for (i <- 1 to n) yield x }
+
+  def duplicateNFunctionalIII[T](n: Int, ls: List[T]) = ls flatMap { List.fill(n)(_) }
+
+  def duplicateNRecursive[T](n: Int, ls: List[T]) = {
+
+    def dulplicateNR[T](ls: List[T]): List[T] = ls match {
+      case Nil     => Nil
+      case x :: xs => (for (i <- 1 to n) yield x).toList ::: dulplicateNR(xs)
+    }
+
+    dulplicateNR(ls)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P15Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P15Spec.scala
@@ -3,5 +3,24 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P15Spec extends FlatSpec with Matchers {
-  
+
+  "duplicateNFunctional" should "return a list with elements in a given list duplicated the given number of times" in {
+    P15.duplicateNFunctional(3, List('a, 'b, 'c, 'c, 'd)) should equal(List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd))
+    P15.duplicateNFunctional(3, Nil) should equal(Nil)
+  }
+
+  "duplicateNFunctionalII" should "return a list with elements in a given list duplicated the given number of times" in {
+    P15.duplicateNFunctionalII(1, List(1, 2, 3, 3, 4)) should equal(List(1, 2, 3, 3, 4))
+    P15.duplicateNFunctionalII(3, List()) should equal(Nil)
+  }
+
+  "duplicateNFunctionalIII" should "return a list with elements in a given list duplicated the given number of times" in {
+    P15.duplicateNFunctionalIII(3, List("a", "b", "c", "c", "d")) should equal(List("a", "a", "a", "b", "b", "b", "c", "c", "c", "c", "c", "c", "d", "d", "d"))
+    P15.duplicateNFunctionalIII(3, List()) should equal(List())
+  }
+
+  "duplicateNRecursive" should "return a list with elements in a given list duplicated the given number of times" in {
+    P15.duplicateNRecursive(3, List('a, 'b, 'c, 'c, 'd)) should equal(List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd))
+    P15.duplicateNRecursive(3, List.empty[Int]) should equal(Nil)
+  }
 }
