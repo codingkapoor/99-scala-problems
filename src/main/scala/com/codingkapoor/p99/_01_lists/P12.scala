b243a48fce0a4e747c383a413ba4cda6928e75f0
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-01 16:47:52 +0530

    Solution and test cases for P12

--- src/main/scala/com/codingkapoor/p99/_01_lists/P12.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P12.scala
@@ -1,5 +1,46 @@
 package com.codingkapoor.p99._01_lists
 
 object P12 {
-  
+
+  def decodeFunctional[T](ls: List[(Int, T)]) = {
+    (ls foldLeft (Nil: List[T])) {
+      (acc, x) =>
+        x match {
+          case (p, q) =>
+            {
+              val j = for (i <- 1 to p) yield q
+              acc ::: j.toList
+            }
+          case _ => Nil
+        }
+    }
+  }
+
+  def decodeFunctionalI[T](ls: List[(Int, T)]) = ls map {
+    case (x, y) => for (i <- 1 to x) yield y
+  } flatten
+
+  def decodeFunctionalII[T](ls: List[(Int, T)]) = ls flatMap {
+    case (x, y) => for (i <- 1 to x) yield y
+  }
+
+  def decodeBuiltin[T](ls: List[(Int, T)]) = ls flatMap {
+    case (p, q) => List.fill(p)(q)
+  }
+
+  def decodeRecursive[T](ls: List[(Int, T)]): List[T] = ls match {
+    case Nil          => Nil
+    case (p, q) :: xs => List.fill(p)(q) ::: decodeRecursive(xs)
+  }
+
+  def decodeTailRecursive[T](ls: List[(Int, T)]): List[T] = {
+
+    def decodeR[T](result: List[T], list: List[(Int, T)]): List[T] = list match {
+      case Nil          => result
+      case (p, q) :: xs => decodeR(result ::: List.fill(p)(q), xs)
+    }
+
+    decodeR(Nil, ls)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P12Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P12Spec.scala
@@ -3,5 +3,35 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P12Spec extends FlatSpec with Matchers {
-  
+
+  "decodeFunctional" should "return decoded a run-length encoded list" in {
+    P12.decodeFunctional(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) should equal(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    P12.decodeFunctional(Nil) should equal(Nil)
+  }
+
+  "decodeFunctionalI" should "return decoded a run-length encoded list" in {
+    P12.decodeFunctionalI(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) should equal(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    P12.decodeFunctionalI(Nil) should equal(Nil)
+  }
+
+  "decodeFunctionalII" should "return decoded a run-length encoded list" in {
+    P12.decodeFunctionalII(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) should equal(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    P12.decodeFunctionalII(Nil) should equal(Nil)
+  }
+
+  "decodeBuiltin" should "return decoded a run-length encoded list" in {
+    P12.decodeBuiltin(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) should equal(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    P12.decodeBuiltin(Nil) should equal(Nil)
+  }
+
+  "decodeRecursive" should "return decoded a run-length encoded list" in {
+    P12.decodeRecursive(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) should equal(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    P12.decodeRecursive(Nil) should equal(Nil)
+  }
+
+  "decodeTailRecursive" should "return decoded a run-length encoded list" in {
+    P12.decodeTailRecursive(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))) should equal(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    P12.decodeTailRecursive(Nil) should equal(Nil)
+  }
+
 }
