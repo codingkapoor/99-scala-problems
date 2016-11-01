2365f944cea742f35f57d0adc18e47ea4247af56
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-01 18:03:52 +0530

    Solution and test cases to P13

--- src/main/scala/com/codingkapoor/p99/_01_lists/P13.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P13.scala
@@ -1,5 +1,28 @@
 package com.codingkapoor.p99._01_lists
 
 object P13 {
-  
+
+  def encodeDirectRecursive[T](ls: List[T]) = {
+
+    def encodeDirectR[T](list: List[T]): List[(Int, T)] = list match {
+      case Nil => Nil
+      case xs  => (xs.takeWhile { _ == xs.head } size, xs.head) :: encodeDirectR(xs.dropWhile { _ == xs.head })
+    }
+
+    encodeDirectR(ls)
+  }
+
+  def encodeDirectRecursiveII[T](ls: List[T]) = {
+
+    def encodeDirectR[T](list: List[T]): List[(Int, T)] = list match {
+      case Nil => Nil
+      case xs  => {
+        val (packed, next) = xs.span { _ == xs.head }
+        (packed size, xs.head) :: encodeDirectR(next)
+      }
+    }
+
+    encodeDirectR(ls)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P13Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P13Spec.scala
@@ -3,5 +3,15 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P13Spec extends FlatSpec with Matchers {
-  
+
+  "encodeDirectRecursive" should "return run-length encoding of a list (direct solution)" in {
+    P13.encodeDirectRecursive(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+    P13.encodeDirectRecursive(Nil) should equal(Nil)
+  }
+
+  "encodeDirectRecursiveII" should "return run-length encoding of a list (direct solution)" in {
+    P13.encodeDirectRecursiveII(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+    P13.encodeDirectRecursiveII(Nil) should equal(Nil)
+  }
+
 }
