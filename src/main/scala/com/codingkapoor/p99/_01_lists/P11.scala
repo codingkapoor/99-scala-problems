16593ee4192fadfe29878411b45be18bf80484b3
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-01 14:38:11 +0530

    Solution and test cases for P11

--- src/main/scala/com/codingkapoor/p99/_01_lists/P11.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P11.scala
@@ -1,5 +1,47 @@
 package com.codingkapoor.p99._01_lists
 
 object P11 {
-  
+
+  import P10._
+
+  def encodeModifiedFunctional[T](ls: List[T]) = {
+
+    if (ls == Nil) Nil
+    else encodeFucntional(ls) map { case (x, y) => if (x == 1) y else (x, y) }
+  }
+
+  def encodeModifiedFunctionalI[T](ls: List[T]) = {
+
+    if (ls == Nil) Nil
+    else (encodeFucntional(ls).reverse foldLeft (Nil: List[Any])) {
+      (acc, x) =>
+        x match {
+          case (p, q) if (p == 1) => q :: acc
+          case (p, q)             => (p, q) :: acc
+        }
+    }
+  }
+
+  def encodeModifiedRecursive[T](ls: List[T]) = {
+
+    def encodeModifiedR[T](list: List[T]): List[Any] = list match {
+      case Nil                      => Nil
+      case (p, q) :: xs if (p == 1) => q :: encodeModifiedR(xs)
+      case (p, q) :: xs             => (p, q) :: encodeModifiedR(xs)
+    }
+
+    encodeModifiedR(encodeFucntional(ls))
+  }
+
+  def encodeModifiedTailRecursive[T](ls: List[T]) = {
+
+    def encodeModifiedR[T](result: List[Any], list: List[T]): List[Any] = list match {
+      case Nil                      => result
+      case (p, q) :: xs if (p == 1) => encodeModifiedR(result ::: List(q), xs)
+      case (p, q) :: xs             => encodeModifiedR(result ::: List((p, q)), xs)
+    }
+
+    encodeModifiedR(Nil, encodeFucntional(ls))
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P11Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P11Spec.scala
@@ -3,5 +3,24 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P11Spec extends FlatSpec with Matchers {
-  
+
+  "encodeModifiedFunctional" should "return modified run-length encoding of a list" in {
+    P11.encodeModifiedFunctional(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal(List((4, 'a), 'b, (2, 'c), (2, 'a), 'd, (4, 'e)))
+    P11.encodeModifiedFunctional(Nil) should equal(Nil)
+  }
+
+  "encodeModifiedFunctionalI" should "return modified run-length encoding of a list" in {
+    P11.encodeModifiedFunctionalI(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal(List((4, 'a), 'b, (2, 'c), (2, 'a), 'd, (4, 'e)))
+    P11.encodeModifiedFunctional(Nil) should equal(Nil)
+  }
+
+  "encodeModifiedRecursive" should "return modified run-length encoding of a list" in {
+    P11.encodeModifiedRecursive(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal(List((4, 'a), 'b, (2, 'c), (2, 'a), 'd, (4, 'e)))
+    P11.encodeModifiedFunctional(Nil) should equal(Nil)
+  }
+
+  "encodeModifiedTailRecursive" should "return modified run-length encoding of a list" in {
+    P11.encodeModifiedTailRecursive(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal(List((4, 'a), 'b, (2, 'c), (2, 'a), 'd, (4, 'e)))
+    P11.encodeModifiedFunctional(Nil) should equal(Nil)
+  }
 }
