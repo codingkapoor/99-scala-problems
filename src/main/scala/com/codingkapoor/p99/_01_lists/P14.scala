355789ac56c0ff4cbf1c4cc966a4dfb321fa619a
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-01 20:14:21 +0530

    Solution and test cases for P14

--- src/main/scala/com/codingkapoor/p99/_01_lists/P14.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P14.scala
@@ -1,5 +1,29 @@
 package com.codingkapoor.p99._01_lists
 
 object P14 {
-  
+
+  def duplicateFunctional[T](ls: List[T]): List[T] = ls map { x => List(x, x) } flatten
+
+  def duplicateFunctionalII[T](ls: List[T]): List[T] = ls flatMap { x => List(x, x) }
+
+  def duplicateFunctionalIII[T](ls: List[T]): List[T] =
+    // 'reverse' function has complexity O(n) next to regular traversal. Hence should be avoided 
+    // (ls.reverse foldLeft (Nil: List[T])) { (acc, x) => x :: x :: acc }
+    (ls foldLeft (Nil: List[T])) { (acc, x) => acc ::: List(x, x) }
+
+  def duplicateRecursive[T](ls: List[T]): List[T] = ls match {
+    case Nil     => Nil
+    case x :: xs => x :: x :: duplicateRecursive(xs)
+  }
+
+  def duplicateTailRecursive[T](ls: List[T]): List[T] = {
+
+    def duplicateR[T](result: List[T], list: List[T]): List[T] = list match {
+      case Nil     => result
+      case x :: xs => duplicateR(result ::: List(x, x), xs)
+    }
+
+    duplicateR(Nil, ls)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P14Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P14Spec.scala
@@ -3,5 +3,29 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P14Spec extends FlatSpec with Matchers {
-  
+
+  "duplicateFunctional" should "return a list with elements duplicated in specified list" in {
+    P14.duplicateFunctional(List('a, 'b, 'c, 'c, 'd)) should equal(List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+    P14.duplicateFunctional(Nil) should equal(Nil)
+  }
+
+  "duplicateFunctionalII" should "return a list with elements duplicated in specified list" in {
+    P14.duplicateFunctionalII(List('a, 'b, 'c, 'c, 'd)) should equal(List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+    P14.duplicateFunctionalII(List()) should equal(Nil)
+  }
+
+  "duplicateFunctionalIII" should "return a list with elements duplicated in specified list" in {
+    P14.duplicateFunctionalIII(List('a', 'b', 'c', 'c', 'd')) should equal(List('a', 'a', 'b', 'b', 'c', 'c', 'c', 'c', 'd', 'd'))
+    P14.duplicateFunctionalIII(List.empty[String]) should equal(Nil)
+  }
+
+  "duplicateRecursive" should "return a list with elements duplicated in specified list" in {
+    P14.duplicateRecursive(List(1, 2, 3, 3, 4)) should equal(List(1, 1, 2, 2, 3, 3, 3, 3, 4, 4))
+    P14.duplicateRecursive(Nil) should equal(List.empty[Int])
+  }
+
+  "duplicateTailRecursive" should "return a list with elements duplicated in specified list" in {
+    P14.duplicateTailRecursive(List("a", "b", "c", "c", "d")) should equal(List("a", "a", "b", "b", "c", "c", "c", "c", "d", "d"))
+    P14.duplicateTailRecursive(Nil) should equal(Nil)
+  }
 }
