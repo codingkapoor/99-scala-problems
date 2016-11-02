752428f1915efd194dca3ca7d7e881ab989a64b9
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-02 13:54:14 +0530

    P17 test cases fixed

--- src/test/scala/com/codingkapoor/p99/_01_lists/P17Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P17Spec.scala
@@ -4,22 +4,22 @@ import org.scalatest.{ FlatSpec, Matchers }
 
 class P17Spec extends FlatSpec with Matchers {
 
-  "splitBuiltin" should "returns a tuple that holds splitted part of a list as it's elements" in {
+  "splitBuiltin" should "return a tuple that holds splitted part of a list as it's elements" in {
     P17.splitBuiltin(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal((List('a, 'b, 'c), List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
     P17.splitBuiltin(3, Nil) should equal((List(), List()))
   }
 
-  "splitBuiltinII" should "returns a tuple that holds splitted part of a list as it's elements" in {
+  "splitBuiltinII" should "return a tuple that holds splitted part of a list as it's elements" in {
     P17.splitBuiltinII(3, List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)) should equal((List(1, 2, 3), List(4, 5, 6, 7, 8, 9, 10, 11)))
     P17.splitBuiltinII(3, List.empty[String]) should equal((List(), List()))
   }
 
-  "splitFunctional" should "returns a tuple that holds splitted part of a list as it's elements" in {
+  "splitFunctional" should "return a tuple that holds splitted part of a list as it's elements" in {
     P17.splitFunctional(3, List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k')) should equal((List('a', 'b', 'c'), List('d', 'e', 'f', 'g', 'h', 'i', 'j', 'k')))
     P17.splitFunctional(3, List()) should equal((List(), List()))
   }
 
-  "splitTailRecursive" should "returns a tuple that holds splitted part of a list as it's elements" in {
+  "splitTailRecursive" should "return a tuple that holds splitted part of a list as it's elements" in {
     P17.splitTailRecursive(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal((List('a, 'b, 'c), List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
     P17.splitTailRecursive(3, Nil) should equal((List(), List()))
   }
