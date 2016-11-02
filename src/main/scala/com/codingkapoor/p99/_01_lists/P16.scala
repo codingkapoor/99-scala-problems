5a9d0f7f54f6d8bfb6594d10e65d5b53149f919b
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-02 13:54:49 +0530

    Solution and test cases for P16

--- src/main/scala/com/codingkapoor/p99/_01_lists/P16.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P16.scala
@@ -1,5 +1,54 @@
 package com.codingkapoor.p99._01_lists
 
 object P16 {
-  
+
+  def dropFunctional[T](n: Int, ls: List[T]) =
+    // ls.zipWithIndex filter { case (x, y) => (y + 1) % n != 0 } map { case (x, y) => x }
+    ls.zipWithIndex.withFilter { case (x, y) => (y + 1) % n != 0 } map { _._1 }
+
+  def dropFunctionalII[T](n: Int, ls: List[T]) = (ls.zipWithIndex foldLeft (Nil: List[T])) {
+    (acc, x) =>
+      x match {
+        case (p, q) if ((q + 1) % n != 0) => acc ::: List(p)
+        case _                            => acc
+      }
+  }
+
+  def dropRecursive[T](n: Int, ls: List[T]): List[T] = {
+
+    def dropR[T](list: List[T]): List[T] = list.splitAt(n) match {
+
+      case (xs, Nil) if (xs.length < n) => xs
+      case (xs, Nil)                    => xs.init
+
+      case (xs, ys)                     => xs.init ::: dropR(ys)
+    }
+
+    dropR(ls)
+  }
+
+  def dropRecursiveII[T](n: Int, ls: List[T]): List[T] = {
+
+    def dropR[T](list: List[T]): List[T] = list match {
+
+      case Nil                   => Nil
+
+      case xs if (xs.length < n) => xs.take(n) ::: dropR(xs.drop(n))
+      case xs                    => xs.take(n).init ::: dropR(xs.drop(n))
+    }
+
+    dropR(ls)
+  }
+
+  def dropTailRecursive[T](n: Int, ls: List[T]) = {
+
+    def dropR[T](t: Int, result: List[T], list: List[T]): List[T] = list match {
+      case Nil                 => result
+      case x :: xs if (t == 1) => dropR(n, result, xs)
+      case x :: xs             => dropR(t - 1, result ::: List(x), xs)
+    }
+
+    dropR(n, Nil, ls)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P16Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P16Spec.scala
@@ -3,5 +3,30 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P16Spec extends FlatSpec with Matchers {
-  
+
+  "dropFunctional" should "return a list with every nth element dropped from a given list" in {
+    P16.dropFunctional(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+    P16.dropFunctional(3, Nil) should equal(Nil)
+  }
+
+  "dropFunctionalII" should "return a list with every nth element dropped from a given list" in {
+    P16.dropFunctionalII(3, List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k')) should equal(List('a', 'b', 'd', 'e', 'g', 'h', 'j', 'k'))
+    P16.dropFunctionalII(3, List()) should equal(Nil)
+  }
+
+  "dropRecursive" should "return a list with every nth element dropped from a given list" in {
+    P16.dropRecursive(3, List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)) should equal(List(1, 2, 4, 5, 7, 8, 10, 11))
+    P16.dropRecursive(3, List.empty[String]) should equal(Nil)
+  }
+
+  "dropRecursiveII" should "return a list with every nth element dropped from a given list" in {
+    P16.dropRecursiveII(3, List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k")) should equal(List("a", "b", "d", "e", "g", "h", "j", "k"))
+    P16.dropRecursiveII(3, Nil) should equal(List())
+  }
+
+  "dropTailRecursive" should "return a list with every nth element dropped from a given list" in {
+    P16.dropTailRecursive(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+    P16.dropTailRecursive(3, Nil) should equal(Nil)
+  }
+
 }
