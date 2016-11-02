1b4a6cde04b955e67e5b3de75228f7abad938764
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-02 16:55:41 +0530

    Solution and test cases for P20

--- src/main/scala/com/codingkapoor/p99/_01_lists/P20.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P20.scala
@@ -1,5 +1,45 @@
 package com.codingkapoor.p99._01_lists
 
-object P20 {
-  
+object P20 extends App {
+
+  def removeAtBuiltin[T](pos: Int, ls: List[T]) = ls.splitAt(pos + 1) match {
+    case (Nil, Nil) => Nil
+    case (x, y)     => (x.init ::: y, x.last)
+  }
+
+  def removeAtBuiltinII[T](pos: Int, ls: List[T]) = {
+    lazy val y = ls.take(pos + 1)
+    if (ls == Nil) Nil else (y.init ::: ls.drop(pos + 1), y.last)
+  }
+
+  // Build a list 'y' without the element at the specified position and then return result
+  // as tuple of y and ls(pos)
+  def removeAtFunctional[T](pos: Int, ls: List[T]) = {
+
+    lazy val y = (ls.zipWithIndex foldLeft (Nil: List[T])) {
+      (acc, x) =>
+        x match {
+          case (p, q) if (q == pos) => acc
+          case (p, q)               => acc ::: List(p)
+        }
+    }
+
+    if (ls == Nil) Nil
+    else (y, ls(pos))
+  }
+
+  def removeAtTailRecursive[T](pos: Int, ls: List[T]) = {
+
+    @scala.annotation.tailrec
+    def removeAtR[T](t: Int, result: List[T], list: List[T]): (List[T], T) = list match {
+      case x :: xs if (t == 1) => (result ::: xs, x)
+      case x :: xs             => removeAtR(t - 1, result ::: List(x), xs)
+    }
+
+    if (ls == Nil) Nil
+    else removeAtR(pos + 1, Nil, ls)
+  }
+
+  println(removeAtBuiltinII(1, List('a, 'b, 'c, 'd)))
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P20Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P20Spec.scala
@@ -3,5 +3,24 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P20Spec extends FlatSpec with Matchers {
-  
+
+  "removeAtBuiltin" should "return the list and the removed element in a tuple" in {
+    P20.removeAtBuiltin(1, List('a, 'b, 'c, 'd)) should equal((List('a, 'c, 'd), 'b))
+    P20.removeAtBuiltin(1, List()) should equal(Nil)
+  }
+
+  "removeAtBuiltinII" should "return the list and the removed element in a tuple" in {
+    P20.removeAtBuiltinII(1, List("a", "b", "c", "d")) should equal((List("a", "c", "d"), "b"))
+    P20.removeAtBuiltinII(1, Nil) should equal(Nil)
+  }
+
+  "removeAtFunctional" should "return the list and the removed element in a tuple" in {
+    P20.removeAtFunctional(1, List('a', 'b', 'c', 'd')) should equal((List('a', 'c', 'd'), 'b'))
+    P20.removeAtFunctional(1, Nil) should equal(List())
+  }
+
+  "removeAtTailRecursive" should "return the list and the removed element in a tuple" in {
+    P20.removeAtTailRecursive(1, List(1, 2, 3, 4)) should equal((List(1, 3, 4), 2))
+    P20.removeAtTailRecursive(1, List.empty[String]) should equal(List())
+  }
 }
