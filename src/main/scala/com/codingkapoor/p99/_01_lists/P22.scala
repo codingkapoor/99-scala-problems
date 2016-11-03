fa0ac8a740cece48e8dd1483ef7156baa4a32390
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-04 00:33:48 +0530

    Solution and test cases for P22

--- src/main/scala/com/codingkapoor/p99/_01_lists/P22.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P22.scala
@@ -1,5 +1,26 @@
 package com.codingkapoor.p99._01_lists
 
 object P22 {
-  
+
+  def rangeBuiltin(begin: Int, end: Int) = {
+    if (end < begin) throw new IllegalArgumentException("Parameter 'end' has to be greater than or equal to parameter 'begin'")
+    List.range(begin, end + 1)
+  }
+
+  def rangeBuiltinII(begin: Int, end: Int) = {
+    if (end < begin) throw new IllegalArgumentException("Parameter 'end' has to be greater than or equal to parameter 'begin'")
+    List.fill(end - begin)(1).scan(begin)(_ + _)
+  }
+
+  def rangeTailRecursive(begin: Int, end: Int) = {
+    if (end < begin) throw new IllegalArgumentException("Parameter 'end' has to be greater than or equal to parameter 'begin'")
+    
+    def rangeR(t: Int, result: List[Int]): List[Int] = {
+      if (t == end + 1) result
+      else rangeR(t + 1, result ::: List(t))
+    }
+
+    rangeR(begin, Nil)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P22Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P22Spec.scala
@@ -3,5 +3,31 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P22Spec extends FlatSpec with Matchers {
-  
+
+  "rangeBuiltin" should "return a list containing all integers within a given range" in {
+    P22.rangeBuiltin(4, 9) should equal(List(4, 5, 6, 7, 8, 9))
+  }
+
+  "rangeBuiltin" should "return IllegalArgumentException if parameter 'end' is lesser than parameter 'begin'" in {
+    val e = intercept[IllegalArgumentException] { P22.rangeBuiltin(9, 4) }
+    e.getMessage.equals("Parameter 'end' has to be greater than or equal to parameter 'begin'")
+  }
+
+  "rangeBuiltinII" should "return a list containing all integers within a given range" in {
+    P22.rangeBuiltinII(4, 9) should equal(List(4, 5, 6, 7, 8, 9))
+  }
+
+  "rangeBuiltinII" should "return IllegalArgumentException if parameter 'end' is lesser than parameter 'begin'" in {
+    val e = intercept[IllegalArgumentException] { P22.rangeBuiltinII(9, 4) }
+    e.getMessage.equals("Parameter 'end' has to be greater than or equal to parameter 'begin'")
+  }
+
+  "rangeTailRecursive" should "return a list containing all integers within a given range" in {
+    P22.rangeTailRecursive(4, 9) should equal(List(4, 5, 6, 7, 8, 9))
+  }
+
+  "rangeTailRecursive" should "return IllegalArgumentException if parameter 'end' is lesser than parameter 'begin'" in {
+    val e = intercept[IllegalArgumentException] { P22.rangeTailRecursive(9, 4) }
+    e.getMessage.equals("Parameter 'end' has to be greater than or equal to parameter 'begin'")
+  }
 }
