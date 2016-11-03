e75fd0ef97f3a8916ab9a8c7f3d342e5b6363cd0
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-03 23:30:25 +0530

    Solution and test cases for P21

--- src/main/scala/com/codingkapoor/p99/_01_lists/P21.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P21.scala
@@ -1,5 +1,37 @@
 package com.codingkapoor.p99._01_lists
 
 object P21 {
-  
+
+  def insertAtBuiltin[T](elem: T, pos: Int, ls: List[T]) = {
+    if (pos < 0) throw new IllegalArgumentException("position should be greater than 0")
+
+    if (ls == Nil) Nil
+    else ls.splitAt(pos) match { case (x, y) => x ::: elem :: y }
+  }
+
+  def insertAtBuiltinII[T](elem: T, pos: Int, ls: List[T]) = {
+    if (pos < 0) throw new IllegalArgumentException("position should be greater than 0")
+
+    if (ls == Nil) Nil
+    else ls.take(pos) ::: elem :: ls.drop(pos)
+  }
+
+  // We get error 'type mismatch; found : List[Any] required: List[T]' for keeping
+  // return type of 'insertAtR' as 'List[T]'. The reason still being unknown to me as of now.
+  def insertAtTailRecursive[T](elem: T, pos: Int, ls: List[T]) = {
+
+    @scala.annotation.tailrec
+    def insertAtR[T](t: Int, result: List[T], list: List[T]): List[Any] = list match {
+      case Nil            => Nil
+      case xs if (t == 0) => result ::: List(elem) ::: xs
+      case xs             => insertAtR(t - 1, result ::: List(xs.head), xs.tail)
+    }
+
+    if (pos < 0) throw new IllegalArgumentException("position should be greater than 0")
+
+    insertAtR(pos, Nil, ls)
+  }
+
+  // folLeft won't be an appropriate solution here since we don't really need to traverse 
+  // the complete list just to insert an element at a given position.
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P21Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P21Spec.scala
@@ -3,5 +3,35 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P21Spec extends FlatSpec with Matchers {
-  
+
+  "insertAtBuiltin" should "return a list with new element added to the given list at specified position" in {
+    P21.insertAtBuiltin('new, 0, List('a, 'b, 'c, 'd)) should equal(List('new, 'a, 'b, 'c, 'd))
+    P21.insertAtBuiltin('new, 1, List()) should equal(List())
+  }
+
+  "insertAtBuiltin" should "throw an IllegalArgumentException if position specified is less than 0" in {
+    val e = intercept[IllegalArgumentException](P21.insertAtBuiltin('new, -1, List('a, 'b, 'c, 'd)))
+    e.getMessage.equals("position should be greater than 0")
+  }
+
+  "insertAtBuiltinII" should "return a list with new element added to the given list at specified position" in {
+    P21.insertAtBuiltinII('x', 1, List('a', 'b', 'c', 'd')) should equal(List('a', 'x', 'b', 'c', 'd'))
+    P21.insertAtBuiltinII('new, 1, Nil) should equal(List())
+  }
+
+  "insertAtBuiltinII" should "throw an IllegalArgumentException if position specified is less than 0" in {
+    val e = intercept[IllegalArgumentException](P21.insertAtBuiltin('new, -1, List('a, 'b, 'c, 'd)))
+    e.getMessage.equals("position should be greater than 0")
+  }
+
+  "insertAtTailRecursive" should "return a list with new element added to the given list at specified position" in {
+    P21.insertAtTailRecursive("new", 1, List("a", "b", "c", "d")) should equal(List("a", "new", "b", "c", "d"))
+    P21.insertAtTailRecursive('new, 1, List.empty[String]) should equal(Nil)
+  }
+
+  "insertAtTailRecursive" should "throw an IllegalArgumentException if position specified is less than 0" in {
+    val e = intercept[IllegalArgumentException](P21.insertAtBuiltin('new, -1, List('a, 'b, 'c, 'd)))
+    e.getMessage.equals("position should be greater than 0")
+  }
+
 }
