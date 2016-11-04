1cf55f2083d14708df72f221ed0a5ff19b4531ca
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-04 21:39:05 +0530

    Solution and test cases for P19

--- src/main/scala/com/codingkapoor/p99/_01_lists/P19.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P19.scala
@@ -1,5 +1,28 @@
 package com.codingkapoor.p99._01_lists
 
 object P19 {
-  
+
+  def rotateBuiltin[T](places: Int, ls: List[T]) = {
+    val xs = ls.splitAt(if (places < 0) ls.size + places else places)
+    xs._2 ::: xs._1
+  }
+
+  def rotateBuiltinII[T](places: Int, ls: List[T]) = {
+    val t = if (places < 0) ls.size + places else places
+    ls.drop(t) ::: ls.take(t)
+  }
+
+  def rotateTailRecursive[T](places: Int, ls: List[T]) = {
+
+    def rotateR[T](counter: Int, result: List[T], list: List[T]): List[T] = list match {
+      case Nil                  => Nil
+      
+      case xs if (counter == 0) => xs ::: result
+      case xs                   => rotateR(counter - 1, result ::: List(xs.head), xs.tail)
+    }
+
+    val t = if (places < 0) ls.size + places else places
+    rotateR(t, Nil, ls)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P19Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P19Spec.scala
@@ -3,5 +3,20 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P19Spec extends FlatSpec with Matchers {
-  
+
+  "rotateBuiltin" should "return a list rotated N places to the left" in {
+    P19.rotateBuiltin(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c))
+    P19.rotateBuiltin(3, List()) should equal(List())
+  }
+
+  "rotateBuiltinII" should "return a list rotated N places to the left" in {
+    P19.rotateBuiltinII(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i))
+    P19.rotateBuiltinII(3, List()) should equal(Nil)
+  }
+
+  "rotateTailRecursive" should "return a list rotated N places to the left" in {
+    P19.rotateTailRecursive(11, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    P19.rotateTailRecursive(3, Nil) should equal(List())
+  }
+
 }
