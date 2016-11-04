d348bed38257621b855e084b26dc873eb10507d6
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-04 16:21:45 +0530

    Solution and test cases for P18

--- src/main/scala/com/codingkapoor/p99/_01_lists/P18.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P18.scala
@@ -1,5 +1,121 @@
 package com.codingkapoor.p99._01_lists
 
-object P18 {
-  
+object P18 extends App {
+
+  def sliceBuiltin[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else ls drop (begin) take (end - begin)
+  }
+
+  def sliceBuiltinII[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else ls slice (begin, end)
+  }
+
+  def sliceBuiltinIII[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else ls.splitAt(begin)._2.splitAt(end - begin)._1
+  }
+
+  def sliceFunctional[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else ls zip (1 to end) filter { _._2 > begin } map { _._1 }
+  }
+
+  def sliceFunctionalII[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else (ls zip (1 to end) filter { _._2 > begin } unzip)._1
+  }
+
+  def sliceFunctionalIII[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else for { (x, y) <- ls zip (1 to end) if y > begin } yield x
+  }
+
+  // This approach however requires complete traversal of list which is really not necessary
+  def sliceFunctionalIV[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else for { (x, y) <- ls.zipWithIndex if (y >= begin && y < end) } yield x
+  }
+
+  def sliceFunctionalV[T](begin: Int, end: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else (ls zip (1 to end) foldLeft (Nil: List[T])) {
+      case (acc, (x, y)) if (y > begin) => acc ::: List(x)
+      case (acc, (x, y))                => acc
+    }
+  }
+
+  def sliceFunctionalVI[T](begin: Int, end: Int, ls: List[T]) = {
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else (ls zip (1 to end) foldRight (Nil: List[T])) {
+      case ((x, y), acc) if (y > begin) => x :: acc
+      case ((x, y), acc)                => acc
+    }
+  }
+
+  def sliceTailRecursive[T](begin: Int, end: Int, ls: List[T]) = {
+
+    def sliceR[T](t: Int, result: List[T], list: List[T]): List[T] = list match {
+      case Nil                           => result
+
+      case xs if (t >= begin && t < end) => sliceR(t + 1, result ::: List(xs.head), xs.tail)
+      case xs                            => sliceR(t + 1, result, xs.tail)
+    }
+
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else sliceR(0, Nil, ls)
+  }
+
+  def sliceTailRecursiveII[T](begin: Int, end: Int, ls: List[T]) = {
+
+    def sliceR[T](result: List[T], list: List[(T, Int)]): List[T] = list match {
+      case (x, y) :: xs if (y >= begin && y < end) => sliceR(result ::: List(x), xs)
+      case (x, y) :: xs                            => sliceR(result, xs)
+
+      case _                                       => result
+    }
+
+    if (ls == Nil) Nil
+    else if (!areParametersValid(begin, end, ls.size))
+      throw new IllegalArgumentException("Parameters 'begin' and/or 'end' are incorrect.")
+
+    else sliceR(Nil, ls.zipWithIndex)
+  }
+
+  def areParametersValid(begin: Int, end: Int, size: Int): Boolean = {
+    if (begin > end || begin < 0 || end > size || end < 0) false
+    else true
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P18Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P18Spec.scala
@@ -3,5 +3,115 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P18Spec extends FlatSpec with Matchers {
-  
+
+  "sliceBuiltin" should "return a list as a slice extracted from the given list" in {
+    P18.sliceBuiltin(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceBuiltin(3, 7, List()) should equal(List())
+  }
+
+  "sliceBuiltin" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceBuiltin(3, 17, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceBuiltinII" should "return a list as a slice extracted from the given list" in {
+    P18.sliceBuiltinII(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceBuiltinII(3, 7, List()) should equal(List())
+  }
+
+  "sliceBuiltinII" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceBuiltinII(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceBuiltinIII" should "return a list as a slice extracted from the given list" in {
+    P18.sliceBuiltinIII(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceBuiltinIII(3, 7, List()) should equal(List())
+  }
+
+  "sliceBuiltinIII" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceBuiltinIII(3, -2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceFunctional" should "return a list as a slice extracted from the given list" in {
+    P18.sliceFunctional(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceFunctional(3, 7, List()) should equal(List())
+  }
+
+  "sliceFunctional" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceFunctional(7, 3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceFunctionalII" should "return a list as a slice extracted from the given list" in {
+    P18.sliceFunctionalII(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceFunctionalII(3, 7, List()) should equal(List())
+  }
+
+  "sliceFunctionalII" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceFunctionalII(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceFunctionalIII" should "return a list as a slice extracted from the given list" in {
+    P18.sliceFunctionalIII(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceFunctionalIII(3, 7, List()) should equal(List())
+  }
+
+  "sliceFunctionalIII" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceFunctionalIII(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceFunctionalIV" should "return a list as a slice extracted from the given list" in {
+    P18.sliceFunctionalIV(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceFunctionalIV(3, 7, List()) should equal(List())
+  }
+
+  "sliceFunctionalIV" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceFunctionalIV(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceFunctionalV" should "return a list as a slice extracted from the given list" in {
+    P18.sliceFunctionalV(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List('d, 'e, 'f, 'g))
+    P18.sliceFunctionalV(3, 7, List()) should equal(List())
+  }
+
+  "sliceFunctionalV" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceFunctionalV(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceFunctionalVI" should "return a list as a slice extracted from the given list" in {
+    P18.sliceFunctionalVI(10, 10, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List())
+    P18.sliceFunctionalVI(3, 7, List()) should equal(List())
+  }
+
+  "sliceFunctionalVI" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceFunctionalVI(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceTailRecursive" should "return a list as a slice extracted from the given list" in {
+    P18.sliceTailRecursive(0, 0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List())
+    P18.sliceTailRecursive(3, 7, List()) should equal(List())
+  }
+
+  "sliceTailRecursive" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceTailRecursive(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
+  "sliceTailRecursiveII" should "return a list as a slice extracted from the given list" in {
+    P18.sliceTailRecursiveII(0, 0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal(List())
+    P18.sliceTailRecursiveII(3, 7, List()) should equal(List())
+  }
+
+  "sliceTailRecursiveII" should "throw an exception for incorrect parameter values 'begin' and/or 'end'" in {
+    val e = intercept[IllegalArgumentException] { P18.sliceTailRecursiveII(-1, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) }
+    e.getMessage.equals("Parameters 'begin' and/or 'end' are incorrect.")
+  }
+
 }
