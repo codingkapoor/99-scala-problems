195ec931ea961794bb974cfe583c25328bec460d
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-05 00:02:28 +0530

    Solution and test cases for P23

--- src/main/scala/com/codingkapoor/p99/_01_lists/P23.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P23.scala
@@ -1,5 +1,28 @@
 package com.codingkapoor.p99._01_lists
 
-object P23 {
-  
+import scala.util.Random
+
+object P23 extends App {
+
+  def randomSelectFunctional[T](r: Int, ls: List[T]): List[T] = {
+    if (ls == Nil) Nil
+    else Seq.fill(r)(Random.nextInt(ls.size)).toList map { i => ls(i) }
+  }
+
+  def randomSelectTailRecursive[T](r: Int, ls: List[T]): List[T] = {
+
+    def randomSelectR[T](t: Int, result: List[T], list: List[T]): List[T] = {
+      val (xs: List[T], x: T) = P20.removeAtBuiltin(Random.nextInt(ls.size), ls)
+
+      if (t == 0) {
+        result
+      } else {
+        randomSelectR(t - 1, x :: result, xs)
+      }
+    }
+
+    if (ls == Nil) Nil
+    else randomSelectR(r, Nil, ls)
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P23Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P23Spec.scala
@@ -3,5 +3,14 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P23Spec extends FlatSpec with Matchers {
-  
+
+  "randomSelectFunctional" should "return a list of extracted a given number of randomly selected elements from a list" in {
+    P23.randomSelectFunctional(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h)).size should equal(3)
+    P23.randomSelectFunctional(3, List()) should equal(Nil)
+  }
+
+  "randomSelectTailRecursive" should "return a list of extracted a given number of randomly selected elements from a list" in {
+    P23.randomSelectTailRecursive(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h)).size should equal(3)
+    P23.randomSelectTailRecursive(3, List()) should equal(Nil)
+  }
 }
