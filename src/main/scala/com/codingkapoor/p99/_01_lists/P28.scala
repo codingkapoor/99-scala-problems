0c054b22c42140707d102d2b0513932ff074ce1c
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-06 00:18:12 +0530

    Solution and test cases for P28 part I

--- src/main/scala/com/codingkapoor/p99/_01_lists/P28.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P28.scala
@@ -1,5 +1,25 @@
 package com.codingkapoor.p99._01_lists
 
-object P28 {
-  
+object P28 extends App {
+
+  def lsortBuiltin[T](ls: List[List[T]]): List[List[T]] = ls sortBy { _.length }
+
+  def lsortBuiltinII[T](ls: List[List[T]]): List[List[T]] = ls sortWith { _.length < _.length }
+
+  /*
+   * Intermediate list using 'zip' with list length. This intermediate list is though not necessary as shown in subsequent solution.
+   * 
+   * scala> val xs = ls zip (ls map {_.size})
+   * xs: List[(List[Symbol], Int)] = List((List('a, 'b, 'c),3), (List('d, 'e),2), (List('f, 'g, 'h),3), (List('d, 'e),2), (List('i, 'j, 'k, 'l),4), (List('m, 'n),2), (List('o),1))
+   * 
+   */
+  def lsortFunctional[T](ls: List[List[T]]): List[List[T]] = {
+    val xs = ls zip (ls map { _.size })
+    (xs.tail foldLeft List(xs.head._1)) { (acc, x) => if (x._2 <= acc.head.size) x._1 :: acc else acc ::: List(x._1) }
+  }
+
+  def lsortFunctionalII[T](ls: List[List[T]]): List[List[T]] = {
+    (ls.tail foldLeft List(ls.head)) { (acc, x) => if (x.size <= acc.head.size) x :: acc else acc ::: List(x) }
+  }
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P28Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P28Spec.scala
@@ -3,5 +3,20 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P28Spec extends FlatSpec with Matchers {
+
+  "lsortBuiltin" should "return sorted list of lists according to length of sublists" in {
+    P28.lsortBuiltin(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(1, 2, 2, 2, 3, 3, 4))
+  }
+
+  "lsortBuiltinII" should "return sorted list of lists according to length of sublists" in {
+    P28.lsortBuiltinII(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(1, 2, 2, 2, 3, 3, 4))
+  }
+
+  "lsortFunctional" should "return sorted list of lists according to length of sublists" in {
+    P28.lsortFunctional(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(1, 2, 2, 2, 3, 3, 4))
+  }
   
+  "lsortFunctionalII" should "return sorted list of lists according to length of sublists" in {
+    P28.lsortFunctionalII(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(1, 2, 2, 2, 3, 3, 4))
+  }
 }
