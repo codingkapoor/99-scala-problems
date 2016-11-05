80be284d9755f14cfe8af0c269e67a0fca605b93
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-05 18:44:40 +0530

    Solution and test cases for P24

--- src/main/scala/com/codingkapoor/p99/_01_lists/P24.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P24.scala
@@ -1,5 +1,13 @@
 package com.codingkapoor.p99._01_lists
 
+import scala.util.Random
+
 object P24 {
+
+  def lottoBuiltin[T](count: Int, max: Int): List[Int] = Random shuffle ((1 to max).toList) take (6)
   
+  def lottoBuiltinII[T](count: Int, max: Int): List[Int] = Stream.continually(Random.nextInt(max)).distinct.take(count).toList
+
+  def lotto[T](count: Int, max: Int): List[Int] = P23.randomSelectFunctional(count, List.range(1, max + 1))
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P24Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P24Spec.scala
@@ -3,5 +3,16 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P24Spec extends FlatSpec with Matchers {
-  
+
+  "lottoBuiltin" should "return a list with N different random numbers from the set 1..M" in {
+    P24.lottoBuiltin(6, 49).size should equal(6)
+  }
+
+  "lottoBuiltinII" should "return a list with N different random numbers from the set 1..M" in {
+    P24.lottoBuiltinII(6, 49).size should equal(6)
+  }
+
+  "lotto" should "return a list with N different random numbers from the set 1..M" in {
+    P24.lotto(6, 49).size should equal(6)
+  }
 }
