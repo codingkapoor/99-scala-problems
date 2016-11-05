3dc8dee27322d7fb0d165610f9dc665f2300f6ed
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-06 01:12:28 +0530

    Solution and test cases for P28 part II

--- src/main/scala/com/codingkapoor/p99/_01_lists/P28.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P28.scala
@@ -1,13 +1,13 @@
 package com.codingkapoor.p99._01_lists
 
-object P28 extends App {
+object P28 {
 
   def lsortBuiltin[T](ls: List[List[T]]): List[List[T]] = ls sortBy { _.length }
 
   def lsortBuiltinII[T](ls: List[List[T]]): List[List[T]] = ls sortWith { _.length < _.length }
 
   /*
-   * Intermediate list using 'zip' with list length. This intermediate list is though not necessary as shown in subsequent solution.
+   * Intermediate list using 'zip' with length of lists. This intermediate list is not necessary as shown in subsequent solution.
    * 
    * scala> val xs = ls zip (ls map {_.size})
    * xs: List[(List[Symbol], Int)] = List((List('a, 'b, 'c),3), (List('d, 'e),2), (List('f, 'g, 'h),3), (List('d, 'e),2), (List('i, 'j, 'k, 'l),4), (List('m, 'n),2), (List('o),1))
@@ -20,6 +20,22 @@ object P28 extends App {
 
   def lsortFunctionalII[T](ls: List[List[T]]): List[List[T]] = {
     (ls.tail foldLeft List(ls.head)) { (acc, x) => if (x.size <= acc.head.size) x :: acc else acc ::: List(x) }
+  }
+
+  def lsortFreqFunctional[T](ls: List[List[T]]): List[List[T]] =
+    (ls sortBy { _.length } groupBy { _.length } values).toList sortBy { _.length } flatten
+
+  /*
+   * Intermediate list using 'zip' with frequency of length of lists.
+   * 
+   * scala> ls zip (k map {x => k.count(_ == x)})
+	 * res151: List[(List[Symbol], Int)] = List((List('a, 'b, 'c),2), (List('d, 'e),3), (List('f, 'g, 'h),2), (List('d, 'e),3), (List('i, 'j, 'k, 'l),1), (List('m, 'n),3), (List('o),1))*
+	 * 
+	 * Then sorting list of lists by count of list size. 
+   */
+  def lsortFreqFunctionalII[T](ls: List[List[T]]): List[List[T]] = {
+    val k = ls map { _.size }
+    ls zip (k map { x => k.count(_ == x) }) sortBy { _._2 } map { _._1 }
   }
 
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P28Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P28Spec.scala
@@ -15,8 +15,16 @@ class P28Spec extends FlatSpec with Matchers {
   "lsortFunctional" should "return sorted list of lists according to length of sublists" in {
     P28.lsortFunctional(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(1, 2, 2, 2, 3, 3, 4))
   }
-  
+
   "lsortFunctionalII" should "return sorted list of lists according to length of sublists" in {
     P28.lsortFunctionalII(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(1, 2, 2, 2, 3, 3, 4))
+  }
+
+  "lsortFreqFunctional" should "return sorted list of lists according to length of sublists" in {
+    P28.lsortFreqFunctional(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(4, 1, 3, 3, 2, 2, 2))
+  }
+
+  "lsortFreqFunctionalII" should "return sorted list of lists according to length of sublists" in {
+    P28.lsortFreqFunctionalII(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) map (_.size) should equal(List(4, 1, 3, 3, 2, 2, 2))
   }
 }
