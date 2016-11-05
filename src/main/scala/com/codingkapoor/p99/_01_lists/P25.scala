4f08220ffd08e2023b78bdb6caa314f8cac8d37b
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-05 20:41:48 +0530

    Solution and test cases for P25

--- src/main/scala/com/codingkapoor/p99/_01_lists/P25.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P25.scala
@@ -1,5 +1,20 @@
 package com.codingkapoor.p99._01_lists
 
+import scala.util.Random
+
 object P25 {
-  
+
+  def randomPermute[T](ls: List[T]): List[T] = P23.randomSelectTailRecursive(ls.length, ls)
+
+  def randomPermuteBuiltin[T](ls: List[T]): List[T] = Random.shuffle(ls).toList
+
+  def randomPermuteBuiltinII[T](ls: List[T]): List[T] = {
+    val permutations = ls.permutations.toList
+    permutations(Random.nextInt(permutations.length))
+  }
+    
+   /*Efficient purely functional algorithms for shuffling are a lot harder.  One
+   is described in http://okmij.org/ftp/Haskell/perfect-shuffle.txt using
+   Haskell. */
+
 }
--- src/test/scala/com/codingkapoor/p99/_01_lists/P25Spec.scala
+++ src/test/scala/com/codingkapoor/p99/_01_lists/P25Spec.scala
@@ -3,5 +3,19 @@ package com.codingkapoor.p99._01_lists
 import org.scalatest.{ FlatSpec, Matchers }
 
 class P25Spec extends FlatSpec with Matchers {
-  
+
+  "randomPermute" should "return a random permutation of the elements of a list" in {
+    P25.randomPermute(List('a, 'b, 'c, 'd, 'e, 'f)).size should equal(6)
+    P25.randomPermute(List()) should equal(Nil)
+  }
+
+  "randomPermuteBuiltin" should "return a random permutation of the elements of a list" in {
+    P25.randomPermuteBuiltin(List('a, 'b, 'c, 'd, 'e, 'f)).size should equal(6)
+    P25.randomPermuteBuiltin(List()) should equal(Nil)
+  }
+
+  "randomPermuteBuiltinII" should "return a random permutation of the elements of a list" in {
+    P25.randomPermuteBuiltinII(List('a, 'b, 'c, 'd, 'e, 'f)).size should equal(6)
+    P25.randomPermuteBuiltinII(List()) should equal(Nil)
+  }
 }
