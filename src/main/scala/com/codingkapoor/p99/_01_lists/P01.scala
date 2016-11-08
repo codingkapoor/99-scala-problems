cdf97cc63a0247fcd952bb51cb80f316fdb65380
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-08 16:00:03 +0530

    problem statement added

--- src/main/scala/com/codingkapoor/p99/_01_lists/P01.scala
+++ src/main/scala/com/codingkapoor/p99/_01_lists/P01.scala
@@ -1,5 +1,14 @@
 package com.codingkapoor.p99._01_lists
 
+/**
+ *
+ * P01 (*) Find the last element of a list.
+ * Example:
+ *
+ * scala> last(List(1, 1, 2, 3, 5, 8))
+ * res0: Int = 8
+ *
+ */
 object P01 {
 
   // Using Scala's "zipWithIndex" method to build tuples of elements with their indexes 
